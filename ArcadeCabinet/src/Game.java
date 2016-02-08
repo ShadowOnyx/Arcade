16d872fd3229225a38204c07935002f48571759d
ShadowOnyx <Jonatan.Oliveira@MU-C1-18.mullen.pvt.k12.co.us>
2016-02-08 09:10:16 -0700

    Basic movement, vertical broken

--- ArcadeCabinet/src/Game.java
+++ ArcadeCabinet/src/Game.java
@@ -5,14 +5,65 @@ import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 @SuppressWarnings("serial")
 public class Game extends JPanel 
 	{
 	int x=0;
 	int y=0;
-	boolean inGame=true;
+	static boolean inGame=true;
+	static boolean right=true;
+	static boolean left=false;
+	static boolean up=false;
+	static boolean down=false;
+	
+	public Game() 
+		{
+		KeyListener listener = new KeyListener() 
+			{
+			@Override
+			public void keyTyped(KeyEvent e) {}
 
+			@Override
+			public void keyPressed(KeyEvent e) 
+				{
+				System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
+				if(KeyEvent.getKeyText(e.getKeyCode()).equals("Left")&&right==false)
+					{
+					up=false;
+					down=false;
+					left=true;	
+					}
+				if(KeyEvent.getKeyText(e.getKeyCode()).equals("Right")&&left==false)
+					{
+					up=false;
+					down=false;
+					right=true;	
+					}
+				if(KeyEvent.getKeyText(e.getKeyCode()).equals("Up")&&down==false)
+					{
+					up=true;
+					right=false;
+					left=true;	
+					}
+				if(KeyEvent.getKeyText(e.getKeyCode()).equals("Down")&&up==false)
+					{
+					right=false;
+					down=true;
+					left=true;	
+					}
+				}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+					
+			}
+		};
+		addKeyListener(listener);
+		setFocusable(true);
+	}
 	
 	
 	@Override
@@ -22,7 +73,7 @@ public class Game extends JPanel
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2d.setColor(Color.black);
-		g2d.fillRect(x, y, 25, 25);	
+		g2d.fillRect(x, y, 25, 25 );	
 		
 		}
 	
@@ -32,23 +83,60 @@ public class Game extends JPanel
 		JFrame frame = new JFrame("SNAEK");
 		Game game = new Game();
 		frame.add(game);
-		frame.setSize(500,500);
+		frame.setSize(480,480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
-		while(true)
+		while(inGame)
 			{
-			Snake.movement();
+			System.out.println("x "+game.x);
+			System.out.println("y "+game.y);
+			game.movement();
 			game.repaint();
+			
 			try
 				{
-				Thread.sleep(1000);
+				Thread.sleep(100);
 				} catch (InterruptedException e)
 				{
 				e.printStackTrace();
 				}
 			}
+		}
+	
+
+	public void movement()
+		{
+		if (x  < 0)
+			{
+			System.out.println("YOU LOSE :^ )");
+			left=false;
+			}
+		if (x  >= 500-49)
+			{
+			System.out.println("YOU LOSE :^ )");
+			right=false;
+			}
+		else if (y  < 0)
+			{
+			System.out.println("YOU LOSE :^ )");	 
+			up=false;
+			}
+		else if (y  >= 500-49)
+			{
+			System.out.println("YOU LOSE :^ )");	
+			down=false;
+			}
+		if(right)
+			x= x+25;
+		if(left)
+			x=x-25;
+		if(up)
+			y=y-25;
+		if(down)
+			y=y+25;
+		
 		}
 
 	}
